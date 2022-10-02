8c66e6114de7a1c331db5c0853565ee1ca01b56a
zYumoto <victorjuanrazer@gmail.com>
2022-10-02 18:14:44 -0300

    add:function to send files button

---
+++ public/src/controller/DropBoxController.js
@@ -0,0 +1,21 @@
+class DropBoxController {
+  constructor() {
+    this.btnSendFileEl = document.querySelector("#btn-send-file");
+    this.inputFilesEl = document.querySelector("#files");
+    this.snackModalEl = document.querySelector("#react-snackbar-root");
+
+    this.initEvents();
+  }
+
+  initEvents() {
+    this.btnSendFileEl.addEventListener("click", (event) => {
+      this.inputFilesEl.click();
+    });
+
+    this.inputFilesEl.addEventListener("change", (event) => {
+      console.log(event.target.files);
+
+      this.snackModalEl.style.display = 'block'
+    });
+  }
+}
