748f1ae076edf401d4d04ef9169a15529be7f697
zYumoto <victorjuanrazer@gmail.com>
2022-10-02 18:52:39 -0300

    add: promise.all and sending files

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -13,9 +13,44 @@ class DropBoxController {
     });
 
     this.inputFilesEl.addEventListener("change", (event) => {
-      console.log(event.target.files);
+      this.uploadTask(event.target.files)
 
       this.snackModalEl.style.display = 'block'
     });
   }
+
+  uploadTask(files) {
+    let promises = [];
+
+    [...files].forEach(file => {
+      promises.push(new Promise((resolve, reject) => {
+        let ajax = new XMLHttpRequest();
+
+        ajax.open('POST', '/upload')
+
+        ajax.onload = event => {
+          try {
+            resolve(JSON.parse(ajax.responseText))
+          } catch (e) {
+            reject(e)
+          }
+        }
+
+        ajax.onerror = event => {
+          reject(event)
+        }
+
+        let formData = new FormData()
+
+        formData.append('input-file', file)
+
+        ajax.send(formData)
+
+      }))
+    })
+
+    return Promise.all(promises)
+  }
+
+
 }
